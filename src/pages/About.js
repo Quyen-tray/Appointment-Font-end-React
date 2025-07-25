82b7b262c27e847a7524a7b118e93ce5fd9b24cf
Trần Quang Việt <hhhyyy584@gmail.com>
2025-07-25 11:30:27 +0700

    FE for patient

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,10 +10,10 @@ function About() {
             <div className="container-fluid bg-primary py-5 hero-header mb-5">
                 <div className="row py-3">
                     <div className="col-12 text-center">
-                        <h1 className="display-3 text-white">About Us</h1>
+                        <h1 className="display-3 text-white">Chúng tôi</h1>
                         <Link to="/" className="h4 text-white">Trang chủ</Link>
                         <i className="far fa-circle text-white px-2"></i>
-                        <Link to="/about" className="h4 text-white">About</Link>
+                        <Link to="/about" className="h4 text-white">Chúng tôi</Link>
                     </div>
                 </div>
             </div>
@@ -31,11 +31,11 @@ function About() {
                     <div className="row g-5">
                         <div className="col-lg-7">
                             <div className="section-title mb-4">
-                                <h5 className="position-relative d-inline-block text-primary text-uppercase">About Us</h5>
-                                <h1 className="display-5 mb-0">The World's Best Dental Clinic That You Can Trust</h1>
+                                <h5 className="position-relative d-inline-block text-primary text-uppercase">Chúng tôi</h5>
+                                <h1 className="display-5 mb-0">Phòng khám nha khoa tốt nhất thế giới mà bạn có thể tin tưởng</h1>
                             </div>
-                            <h4 className="text-body fst-italic mb-4">Diam dolor diam ipsum sit...</h4>
-                            <p className="mb-4">Tempor erat elitr rebum at clita...</p>
+                            <h4 className="text-body fst-italic mb-4">Chăm sóc toàn diện...</h4>
+                            <p className="mb-4">Tận tâm từng phút, vững bước cùng bạn!</p>
                             <div className="row g-3">
                                 <motion.div
                                     className="col-sm-6"
@@ -44,8 +44,8 @@ function About() {
                                     transition={{ delay: 0.3, duration: 0.5 }}
                                     viewport={{ once: true }}
                                 >
-                                    <h5 className="mb-3"><i className="fa fa-check-circle text-primary me-3"></i>Award Winning</h5>
-                                    <h5 className="mb-3"><i className="fa fa-check-circle text-primary me-3"></i>Professional Staff</h5>
+                                    <h5 className="mb-3"><i className="fa fa-check-circle text-primary me-3"></i>Giải thưởng đạt được</h5>
+                                    <h5 className="mb-3"><i className="fa fa-check-circle text-primary me-3"></i>Đội ngũ chuyên nghiệp</h5>
                                 </motion.div>
 
                                 <motion.div
@@ -55,8 +55,8 @@ function About() {
                                     transition={{ delay: 0.6, duration: 0.5 }}
                                     viewport={{ once: true }}
                                 >
-                                    <h5 className="mb-3"><i className="fa fa-check-circle text-primary me-3"></i>24/7 Opened</h5>
-                                    <h5 className="mb-3"><i className="fa fa-check-circle text-primary me-3"></i>Fair Prices</h5>
+                                    <h5 className="mb-3"><i className="fa fa-check-circle text-primary me-3"></i>Mở cửa 24/7</h5>
+                                    <h5 className="mb-3"><i className="fa fa-check-circle text-primary me-3"></i>Giá cả hợp lý</h5>
                                 </motion.div>
                             </div>
 
@@ -100,7 +100,7 @@ function About() {
                     <div className="bg-primary p-5">
                         <form className="mx-auto" style={{ maxWidth: '600px' }}>
                             <div className="input-group">
-                                <input type="text" className="form-control border-white p-3" placeholder="Your Email" />
+                                <input type="text" className="form-control border-white p-3" placeholder="Email" />
                                 <button className="btn btn-dark px-4">Sign Up</button>
                             </div>
                         </form>
